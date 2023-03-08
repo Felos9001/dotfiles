d269f84a6bc6ddfecb9fa56adf914c2dff618396
felos9001 <90056233+Felos9001@users.noreply.github.com>
2023-03-08 12:46:19 -0500

    added .config

---
+++ .config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,12 @@
+return {
+  -- adds catppuccin
+  { "catppuccin/nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
