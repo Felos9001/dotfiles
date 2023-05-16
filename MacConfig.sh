7973fb630305a9784ee234139fe6e6855bea85af
Felipe Solorzano <90056233+Felos9001@users.noreply.github.com>
2023-05-16 15:20:32 -0400

    Update MacConfig.sh

--- MacConfig.sh
+++ MacConfig.sh
@@ -20,16 +20,16 @@ Photos=$(dock_item /System/Applications/Photos.app)
 Calendar=$(dock_item /System/Applications/Calendar.app)
 Obsidian=$(dock_item /Applications/Obsidian.app)
 VScode=$(dock_item /Applications/vscode.app)
+Safari=$(dock_item /Applications/Safari.app)
 warp=$(dock_item /Applications/warp.app)
 Xcode=$(dock_item /Applications/Xcode.app)
 AppStore=$(dock_item /Applications/App\ Store.app)
 PhotoBooth=$(dock_item /Applications/Photo\ Booth.app)
-OperaGX=$(dock_item /Applications/Opera\ GX.app)
 Messages=$(dock_item /Applications/Messages.app)
 FaceTime=$(dock_item /Applications/FaceTime.app)
 SystemPreferences=$(dock_item /System/Applications/System\ Preferences.app)
  
-sudo su $LOGGED_USER -c "defaults write com.apple.dock persistent-apps -array '$Launchpad' '$Photos' '$Calendar' '$Obsidian' '$VScode' '$warp' '$Xcode' '$AppStore' '$PhotoBooth' '$OperaGX' '$Messages' '$Facetime' '$SystemPreferences'"
+sudo su $LOGGED_USER -c "defaults write com.apple.dock persistent-apps -array '$Launchpad' '$Photos' '$Calendar' '$Obsidian' '$VScode' '$warp' '$Xcode' '$AppStore' '$PhotoBooth' '$Safari' '$Messages' '$Facetime' '$SystemPreferences'"
 
 
 defaults write com.apple.dock "orientation" -string "bottom"
@@ -40,6 +40,8 @@ defaults write com.apple.finder "FXDefaultSearchScope" -string "SCcf"
 defaults write com.apple.universalaccess "showWindowTitlebarIcons" -bool "true"
 defaults write com.apple.finder "ShowRemovableMediaOnDesktop" -bool "true" && killall Finder
 defaults write com.apple.finder "ShowMountedServersOnDesktop" -bool "true" && killall Finder
+sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '/Users/felos/Pictures/Cathedral.jpeg'"
+
 defaults write com.apple.menuextra.clock Show24Hour 0
 
 killall Dock
